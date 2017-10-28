75f3e8c5d40766a36f3de3aca70bfe0c4d406759
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-28 17:05:06 +0300

    Add files via upload

---
+++ OOP Introduction - Defining Classes/03. Opinion Poll/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+    {
+       
+        static void Main(string[] args)
+      {
+
+        int n = int.Parse(Console.ReadLine());
+        Person[] persons = new Person[n];
+      //  Person[] persons = new Person[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            var parts = input.Split(' ').ToArray();
+            var name = parts[0];
+            var age = int.Parse(parts[1]);
+            persons[i] = new Person(name, age);
+        }
+        List<Person> result = persons.ToList();
+        foreach (var item in result.OrderBy(x=>x.Name))
+        {
+           
+            if (item.Age>30)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+        }
+       
+
+
+ 
+
+    }
+
+}
+
+
