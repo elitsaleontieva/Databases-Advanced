18e0babc3b4d2e8b0b5200499cf755605193c57c
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-28 18:16:21 +0300

    Create StartUp.cs

---
+++ Lab- Defining Classes/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp
+{
+    class Program
+    {
+        static void Main()
+        {
+            BankAccount acc = new BankAccount();
+
+            acc.ID = 1;
+            acc.Deposit(15);
+            acc.Withdraw(5);
+
+            Console.WriteLine($"Account {acc.ID}, balance = {acc.Balance:f2}.");
+        }
+
+    }
+}
