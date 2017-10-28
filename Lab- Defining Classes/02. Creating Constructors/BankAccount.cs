a41947caa906f0b8c8028f759f585c8175033c3d
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-28 18:16:46 +0300

    Add files via upload

---
+++ Lab- Defining Classes/02. Creating Constructors/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication85
+{
+    class BankAccount
+    {
+        public int ID { get; set; }
+
+        public decimal Balance { get; set; }
+
+        decimal amount;
+
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            this.Balance -= amount;
+        }
+    }
+}
