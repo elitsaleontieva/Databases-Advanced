458a2b2b725a18f53618067e8b9719c1508f81c3
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-28 18:03:59 +0300

    Create BankAccount.cs

---
+++ 01. Define Bank Account Class/BankAccount.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication85
+{
+    class BankAccount
+    {
+        public int ID { get; set; }
+
+        public int Balance { get; set; }
+
+    }
+}
