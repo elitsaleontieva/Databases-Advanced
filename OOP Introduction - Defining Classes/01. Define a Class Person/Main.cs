0f0238f6addb8939858fc563897a98028fcbaa00
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-28 15:36:27 +0300

    Create Main.cs

---
+++ OOP Introduction - Defining Classes/01. Define a Class Person/Main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+    {
+       
+        static void Main(string[] args)
+{
+            Type personType = typeof(Person);
+            PropertyInfo[] properties = personType.GetProperties
+                (BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(properties.Length);
+        }
+
+    }
