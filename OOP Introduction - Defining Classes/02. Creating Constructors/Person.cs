76d67d5ca27d1722ab5ac162e0f13802a87d7fe2
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-28 16:32:54 +0300

    Add files via upload

---
+++ OOP Introduction - Defining Classes/02. Creating Constructors/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Person
+    {
+    public Person() // This is the private constructor.
+    {
+        this.Age = 1;
+        this.Name = "No name";
+
+       
+
+    }
+
+    public Person(int age) // This is the private constructor.
+    {
+        this.Age = age;
+        this.Name = "No name";
+  
+    }
+    public Person(string name, int age) // This is the private constructor.
+    {
+        this.Age = age;
+        this.Name = name;
+
+    }
+
+
+    public String Name { get; set; }
+    public int Age { get; set; }
+
+}
+
