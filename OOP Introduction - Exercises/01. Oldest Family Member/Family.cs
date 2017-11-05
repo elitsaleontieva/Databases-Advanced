2c032b984d2ace5f623a36a71b773f594e1ae475
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-06 01:14:54 +0200

    Add files via upload

---
+++ OOP Introduction - Exercises/01. Oldest Family Member/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Oldest_Family_Member
+{
+    class Family : Person
+    {
+        public List<Person> People= new List<Person>();
+
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            this.People.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.People.OrderByDescending(x=>x.Age).First();
+        }
+    }
+}
---
+++ OOP Introduction - Exercises/01. Oldest Family Member/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Oldest_Family_Member
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
