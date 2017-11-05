8b9d6e8c32cbc62dc1b7d41c57755c42b836467d
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-06 00:31:58 +0200

    Create Worker.cs

---
+++ OOP Introduction - Inheritance/Mankind/Worker.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mankind
+{
+    class Worker : Human
+    {
+        private double weekSalary;
+        private double workHoursPerDay;
+
+        public Worker(string firstName, string lastName, double weekSalary, double workHoursPerDay)
+            : base(firstName, lastName)
+        {
+            this.LastName = lastName;
+            this.WeekSalary = weekSalary;
+            this.WorkHoursPerDay = workHoursPerDay;
+        }
+      
+
+      
+
+        public double WeekSalary
+        {
+            get
+            {
+                return this.weekSalary;
+            }
+
+            set
+            {
+                if (value <= 10)
+                {
+                    throw new ArgumentException("Expected value mismatch! Argument: weekSalary");
+                }
+                this.weekSalary = value;
+            }
+        }
+
+        public double WorkHoursPerDay
+        {
+            get
+            {
+                return this.workHoursPerDay;
+            }
+
+            set
+            {
+                if (value < 1 || value > 12)
+                {
+                    throw new ArgumentException("Expected value mismatch! Argument: workHoursPerDay");
+                }
+                this.workHoursPerDay = value;
+            }
+        }
+
+       
+
+      
+    }
+}
+    
