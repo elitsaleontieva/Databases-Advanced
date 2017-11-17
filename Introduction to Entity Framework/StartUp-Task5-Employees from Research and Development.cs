0d76b55e4568adfbcba9ef2207018510884bd1d8
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-17 16:41:26 +0200

    Update StartUp-Task5-Employees from Research and Development.cs

--- Introduction to Entity Framework/StartUp-Task5-Employees from Research and Development.cs
+++ Introduction to Entity Framework/StartUp-Task5-Employees from Research and Development.cs
@@ -1,1 +1,35 @@
+using System;
+using P02_DatabaseFirst.Data;
+using P02_DatabaseFirst.Data.Models;
+using System.Linq;
 
+
+namespace P02_DatabaseFirst
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new SoftUniContext())
+            {
+                var employees=db.Employees
+                    .Where(e=>e.Department.Name=="Research and Development")
+                    .OrderBy(e=>e.Salary).ThenByDescending(e=>e.FirstName)
+                    .Select(e=> new
+                    {
+
+                        Name=$"{e.FirstName} {e.LastName}",
+                        e.Salary,
+                        DepartmentName=e.Department.Name
+                    });
+
+                foreach (var e in employees)
+                {
+                    Console.WriteLine($"{e.Name} from {e.DepartmentName} - ${e.Salary:f2}");
+                }
+                   
+            } 
+        }
+    }
+}
