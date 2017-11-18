d05413efbd4eb5096c981b79eb749f6386afe3a0
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-18 20:12:53 +0200

    Add files via upload

---
+++ Code-First/1. Hospital Database/Data/Models/Patient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+        //инициализираме колекциите в конструктор след като сме направили всичко
+
+        public Patient()
+        {
+            Prescriptions = new List<PatientMedicament>();
+
+            Diagnoses = new List<Diagnose>();
+
+            Visitations = new List<Visitation>();
+        }
+
+        public int PatientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Email { get; set; }
+
+        public bool HasInsurance { get; set; }
+
+
+        public ICollection<Visitation> Visitations { get; set; }
+
+        public ICollection<Diagnose> Diagnoses { get; set; }
+
+        public ICollection<PatientMedicament> Prescriptions { get; set; }
+    }
+}
