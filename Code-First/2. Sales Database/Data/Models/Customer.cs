ced42bc0b813b9739c4fbec113cfb3697cc0fd88
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-19 03:45:21 +0200

    Add files via upload

---
+++ Code-First/2. Sales Database/Data/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace P03_SalesDatabase.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Customer
+    {
+        public Customer()
+        {
+            Sales = new List<Sale>();
+        }
+
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CreaditCardNumber { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+    }
+}
---
+++ Code-First/2. Sales Database/Data/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace P03_SalesDatabase.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Product
+    {
+        public Product()
+        {
+            Sales = new List<Sale>();
+        }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+    }
+}
