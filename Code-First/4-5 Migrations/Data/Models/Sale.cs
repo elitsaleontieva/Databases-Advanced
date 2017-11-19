297a5695e1bac3573f345340fb0e9cbe15e69026
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-19 04:14:17 +0200

    Add files via upload

---
+++ Code-First/4-5 Migrations/Data/Models/Sale.cs
@@ -0,0 +1,27 @@
+﻿namespace P03_SalesDatabase.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Sale
+    {
+
+        public int SaleId { get; set; }
+        public DateTime Date { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int CustomerId { get; set; } //?
+        public Customer Customer { get; set; } //?
+
+        public int StoreId { get; set; }
+        public Store Store { get; set; } //?
+
+
+
+    }
+}
---
+++ Code-First/4-5 Migrations/Data/Models/Store.cs
@@ -0,0 +1,22 @@
+﻿namespace P03_SalesDatabase.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Store
+    {
+        public Store()
+        {
+            Sales = new List<Sale>(); //this.Sales = new HashSet<Sale>();
+        }
+
+        public int StoreId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Sale> Sales { get; set; }
+
+
+    }
+}
