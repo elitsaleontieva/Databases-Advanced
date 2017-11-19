fa442eedcab760624b4970e3ee1b2686525dd092
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-19 04:15:17 +0200

    Add files via upload

---
+++ Code-First/4-5 Migrations/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace P03_SalesDatabase
+{ 
+    using System;
+    using P03_SalesDatabase.Data;
+    using P03_SalesDatabase.Data.Models;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+            using (var db = new SalesContext())
+            {
+                db.Database.EnsureDeleted();
+              
+            }
+        }
+}
+}
