32c7392a4a95a3941d6c85dec4059e10d6386ba8
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-21 01:14:26 +0200

    Add files via upload

---
+++ Entity Relations/3. Football Betting/Data/Models/GameResult.cs
@@ -0,0 +1,15 @@
+﻿
+namespace P03_FootballBetting.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public enum GameResult
+    {
+        HomeTeamWin = 1,
+        AwayTeamWin = 2,
+        Draw = 0
+    }
+}
---
+++ Entity Relations/3. Football Betting/Data/Models/Town.cs
@@ -0,0 +1,19 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    using System.Collections.Generic;
+
+    public class Town
+    {
+        public Town()
+        {
+            Teams = new List<Team>();
+        }
+        public int TownId { get; set; }
+        public string Name { get; set; }
+        public Country Country { get; set; }
+        public int CountryId { get; set; }
+
+        public ICollection<Team> Teams { get; set; }
+
+    }
+}
