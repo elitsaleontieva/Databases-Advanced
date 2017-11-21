cdb0c955db5a2a2c3026cedb3b44d9a7a46f04cc
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-21 15:22:58 +0200

    Update Sale.cs

--- Code-First/4-5 Migrations/Data/Models/Sale.cs
+++ Code-First/4-5 Migrations/Data/Models/Sale.cs
@@ -15,11 +15,11 @@
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
-        public int CustomerId { get; set; } //?
-        public Customer Customer { get; set; } //?
+        public int CustomerId { get; set; } 
+        public Customer Customer { get; set; } 
 
         public int StoreId { get; set; }
-        public Store Store { get; set; } //?
+        public Store Store { get; set; } 
 
 
 
