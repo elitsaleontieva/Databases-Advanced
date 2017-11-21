0c889c13f09c446923dabc8a5cad15f8db0636df
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-21 04:10:10 +0200

    Create Course.cs

---
+++ Entity Relations/1. Student System/Data/Models/Course.cs
@@ -0,0 +1,33 @@
+
+
+namespace P01_StudentSystem.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Course
+    {
+        public Course()
+        {
+            StudentsEnrolled = new List<StudentCourse>();
+            Resources = new List<Resource>();
+            HomeworkSubmissions = new List<Homework>();
+        }
+
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+
+
+        public ICollection<Resource> Resources { get; set; }
+        public ICollection<StudentCourse> StudentsEnrolled { get; set; }
+        public ICollection<Homework> HomeworkSubmissions { get; set; }
+
+
+
+    }
+}
