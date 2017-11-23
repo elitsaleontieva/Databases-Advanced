71f467c9d081009461bd236bc1e2e60a5d23b082
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-11-24 01:16:27 +0200

    Create BankAccount.cs

---
+++ Exercise- Advanced Relations/Data/Models/BankAccount.cs
@@ -0,0 +1,20 @@
+namespace P01_BillsPaymentSystem.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BankAccount
+    {
+       
+        public int BankAccountId { get; set; }
+        public decimal Balance { get; set; }
+        public string BankName { get; set; }
+        public string SwiftCode { get; set; }
+
+        public int PaymentMethodId { get; set; }  
+        public PaymentMethod PaymentMethod { get; set; } ]
+
+
+    }
+}
