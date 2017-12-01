490eb0e2e98caf648fc6083c394e6ba3772d95c1
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-12-01 05:07:23 +0200

    Create StartUp.cs

---
+++ Code-First/2. Sales Database/3.Hospital Database Modification/P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,18 @@
+namespace P01_HospitalDatabase
+{
+    using System;
+    using P01_HospitalDatabase.Data;
+    using P01_HospitalDatabase.Data.Models;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new HospitalContext())
+            {
+                db.Database.EnsureCreated();
+            }
+
+        }
+    }
+}
