0e77b3ec7ee8c2428968dbd3ee764da519c8dffe
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-12-01 05:20:12 +0200

    Add files via upload

---
+++ Code-First/1-2 Hospital Database and Hospital Database Modification/P01_HospitalDatabase.Data/HospitalContext.cs
@@ -0,0 +1,63 @@
+﻿
+
+namespace P01_HospitalDatabase.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.EntityFrameworkCore;
+    using P01_HospitalDatabase.Data.Models;
+    using P01_HospitalDatabase.Data.EntityConfiguration;
+
+    public class HospitalContext : DbContext
+    {
+
+            public HospitalContext()
+            {
+
+            }
+
+            public HospitalContext(DbContextOptions options)
+                : base(options)
+            {
+
+            }
+
+
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Visitation> Visitations { get; set; }
+        public DbSet<Diagnose> Diagnoses { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<PatientMedicament> Prescriptions { get; set; }
+
+
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PatientConfiguration());
+            modelBuilder.ApplyConfiguration(new VisitationConfiguration());
+            modelBuilder.ApplyConfiguration(new DiagnosesConfiguration());
+            modelBuilder.ApplyConfiguration(new MedicamentConfiguration());
+            modelBuilder.ApplyConfiguration(new PatientMedicamentConfiguration());
+
+
+
+
+        }
+    }
+    }
+
