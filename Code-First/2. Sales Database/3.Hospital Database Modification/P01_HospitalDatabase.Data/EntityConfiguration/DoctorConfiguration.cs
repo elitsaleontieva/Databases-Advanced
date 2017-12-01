e339a7e4f1daf5522fbe40705139c9459d57141f
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-12-01 05:08:56 +0200

    Add files via upload

---
+++ Code-First/2. Sales Database/3.Hospital Database Modification/P01_HospitalDatabase.Data/EntityConfiguration/DoctorConfiguration.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace P01_HospitalDatabase.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using P01_HospitalDatabase.Data.Models;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+    {
+        public void Configure(EntityTypeBuilder<Doctor> builder)
+        {
+
+            
+
+            
+        }
+    }
+}
---
+++ Code-First/2. Sales Database/3.Hospital Database Modification/P01_HospitalDatabase.Data/EntityConfiguration/PatientConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace P01_HospitalDatabase.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using P01_HospitalDatabase.Data.Models;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.HasKey(e => e.PatientId);
+
+            builder.Property(e => e.FirstName)
+                .IsUnicode()
+                .HasMaxLength(50);
+
+            builder.Property(e => e.LastName)
+                .IsUnicode()
+                .HasMaxLength(50);
+
+            builder.Property(e => e.Address)
+                .IsUnicode()
+                .HasMaxLength(250);
+
+            builder.Property(e => e.Email)
+                .IsUnicode(false)
+                .HasMaxLength(80);
+
+            
+            
+
+
+
+        }
+    }
+}
